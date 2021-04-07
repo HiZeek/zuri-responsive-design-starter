7ab7b3bd6b0b00dc2cdc42d34422db477f89a582
zeek-tech <idibiaisaac3@gmail.com>
2021-04-07 11:42:22 -0700

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    // Mobile nav
+
+    $('.js--mobile-nav').click(function() {
+        var nav = $('.js--nav-links');
+
+        nav.slideToggle(200);
+    });
+
+});
