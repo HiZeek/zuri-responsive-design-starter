a18df700d7b8508d95e0340e77526a97e14dfb27
zeek-tech <idibiaisaac3@gmail.com>
2021-04-08 07:55:58 -0700

    opening mobile nav

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-// $(document).ready(function() {
-
-//     // Mobile nav
-
-//     $('.js--mobile-nav').click(function() {
-//         var nav = $('.js--nav-links');
-
-//         nav.slideToggle(200);
-//     });
-
-// });
-
-// function myFunction() {
-//     var x = document.getElementByClass("nav-links-mobile");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
-
 const openMobileNav = document.querySelector('.mobile-nav-open')
 const mobileNavLinks = document.querySelector('.nav-links')
 const closeMobileNav = document.querySelector('.mobile-nav-close')
 
 // open menu
 
-openMobileNav.addEventListener('click', (event) => {
+openMobileNav.addEventListener('click', () => {
     mobileNavLinks.classList.toggle('show-mobileNavLinks')
-    console.log(event)
 });
 
 
