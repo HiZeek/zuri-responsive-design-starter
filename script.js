41d976c7f3169f34613597d0c2db6eb2bde905c0
zeek-tech <idibiaisaac3@gmail.com>
2021-04-08 04:26:17 -0700

    mobile nav

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    // Mobile nav
+//     // Mobile nav
 
-    $('.js--mobile-nav').click(function() {
-        var nav = $('.js--nav-links');
+//     $('.js--mobile-nav').click(function() {
+//         var nav = $('.js--nav-links');
 
-        nav.slideToggle(200);
-    });
+//         nav.slideToggle(200);
+//     });
 
+// });
+
+// function myFunction() {
+//     var x = document.getElementByClass("nav-links-mobile");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+const openMobileNav = document.querySelector('.mobile-nav-open')
+const mobileNavLinks = document.querySelector('.nav-links-mobile')
+// const closeMobileNav = document.querySelector('.mobile-nav-close')
+
+// open menu
+
+openMobileNav.addEventListener('click', () => {
+    mobileNavLinks.classList.add('.show-mobileNavLinks')
 });
+
+// close menu
+
+// closeMobileNav.addEventListener('click', () => {
+//     mobileNavLinks.classList.remove('.show-mobileNavLinks')
+// });
