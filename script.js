0f2a5a96e18ec8eed17578434a4b404b585101b9
zeek-tech <idibiaisaac3@gmail.com>
2021-04-08 13:23:32 -0700

    final change

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const openMobileNav = document.querySelector('.mobile-nav-open')
 const mobileNavLinks = document.querySelector('.nav-links')
-const closeMobileNav = document.querySelector('.mobile-nav-close')
-
-// open menu
 
 openMobileNav.addEventListener('click', () => {
     mobileNavLinks.classList.toggle('show-mobileNavLinks')
 });
-
-
-
-// close menu
-
-// closeMobileNav.addEventListener('click', () => {
-//     mobileNavLinks.classList.remove('.show-mobileNavLinks')
-// });
